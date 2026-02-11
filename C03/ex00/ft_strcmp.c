6d734b5bf46a1bcadebad67df51519ba45e769fb
Aimée Doyle <aimee@Aimees-MacBook-Air.local>
2026-02-11 21:55:39 +0000

    aaa

--- C03/ex00/ft_strcmp.c
+++ C03/ex00/ft_strcmp.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aimdoyle <aimdoyle@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aimee <aimee@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/02 23:21:07 by aimdoyle          #+#    #+#             */
-/*   Updated: 2026/02/03 15:50:24 by aimdoyle         ###   ########.fr       */
+/*   Updated: 2026/02/05 12:21:52 by aimee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,20 @@ int ft_strcmp(char *s1, char *s2)
     int i;
 
     i = 0;
-    while(s1[i] != '\0' && s2[i] != '\0')
+    while(s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
     {
-        if(s1[i] != s2[i])
-        {
-            return (s1[i] - s2[i]);
-		}
         i++;
     }
-    return(s1[i] - s2[i]);
+    return (s1[i] - s2[i]);
 }
-// #include <stdio.h> 
-// int main(void)
-// {
-// 	char x[] = "123";
-// 	char y[] = "123";
-// 	char z[] = "124";
-// 	printf("%d\n", ft_strcmp(x, y));
-// 	printf("%d\n", ft_strcmp(x, z));
-//     printf("%d\n", ft_strcmp(z, x));
-// }
+#include <stdio.h>  //library for printf
+int main(void) //main function
+{
+    char *a = "abc"; //char varible for s1
+    char *b = "abd"; //char varible for s2
+
+    int result = ft_strcmp(a, b); //int type result for ASCII comparison!
+
+    printf("%d\n", result); //print number, result
+    return (0);
+}
